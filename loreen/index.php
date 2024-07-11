8830bad55d10cff97a49a1039c162e9955210b64
Yoann PIKS-L <50371128+PuxianAlHazred@users.noreply.github.com>
2024-07-11 14:55:44 +0200

    Add files via upload

---
+++ loreen/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	<!-- Meta de qualité -->
+		<title>LOREEN - LA COUR DES MIRACLES</title>
+		<meta name="description" content="Loreen est le tatoueur graphique du studio. Il aime garder le côté crayonné du croquis dans ses tatouages  tout en y mêlant couleurs aquarelles et dessin réaliste ou géométrique." />
+		<meta name="author" content="Piks-L" />	
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, minimal-ui"/>
+		 <meta charset="UTF-8">
+	<!-- Meilleur Favicon européen en 2017 élu à l'unanimité -->
+		<meta name="apple-mobile-web-app-title" content="LA COUR DES MIRACLES">
+		<meta name="application-name" content="LA COUR DES MIRACLES">
+		<meta name="msapplication-TileColor" content="#000000">
+		<meta name="msapplication-TileImage" content="../assets/img/favicons/fmstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		<link rel="shortcut icon" href="http://www.lacourdesmiracles.tattoo/assets/img/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/fapple-touch-icon.png">
+		<link rel="icon" type="image/png" href="../assets/img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="../assets/img/favicons/ffavicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="../assets/img/favicons/fmanifest.json">
+		<link rel="mask-icon" href="../assets/img/favicons/fsafari-pinned-tab.svg" color="#000000">
+	<!-- Les livres de la librairie à rendre avant mercredi -->	
+		<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/bootstrap.extension.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/swiper.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/simplelightbox.css" rel="stylesheet" type="text/css" />
+	<!-- Ma collection de Dragon Ball -->	
+		<link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/resette.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/stylette.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/mobile.css" rel="stylesheet" type="text/css" />
+	<!-- Jeans tout neuf pour une belle soirée -->			
+		<script src="../assets/js/modernizr.js"></script>
+	<!-- Mes informations Tinder -->		
+		<meta property="og:site_name" content="LA COUR DES MIRACLES"/>
+		<meta property="og:title" content="LOREEN - LA COUR DES MIRACLES"/>
+		<meta property="og:type"   content="profile" /> 
+		<meta property="og:image" content="http://www.lacourdesmiracles.tattoo/loreen/img/loreen-profil.jpg"/>
+		<meta property="og:description" content="C’est aprés quelques années en tant que graphiste qu’il se lancera dans l’aventure du tatouage sous l’impulsion de Piero en devenant  son apprenti. Son style est souvent qualifié de graphique vu qu’il aime les dessins gardant le côté crayonné du croquis et les couleurs aquarelles. Aujourd’hui il se lance dans des pièces plus ambitieuses en amenant un dessin presque réaliste dans ses compositions."/>
+	<!-- Description de la cible à abattre ! -->			
+		<script type="text/javascript">
+			var _paq = _paq || [];
+			_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+			_paq.push(["setCookieDomain", "*.lacourdesmiracles.tattoo"]);
+			_paq.push(["setDomains", ["*.lacourdesmiracles.tattoo"]]);
+			_paq.push(["disableCookies"]);
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+				(function() {
+					var u="//piwik.piks-l.com/";
+					_paq.push(['setTrackerUrl', u+'piwik.php']);
+					_paq.push(['setSiteId', '3']);
+					var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+					g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+				})();
+		</script>
+	<!-- Pour les cassos -->	
+		<noscript><p><img src="//piwik.piks-l.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>	
+	</head>
+	<body>
+		<!-- Ma collection de Pin's -->
+
+		<!-- Casquette EKKO ... une tuerie ! -->
+		<?php include("../header.php"); ?>
+		<!-- Mon polo LACOSTE -->
+		<div id="content-block">
+			<!-- PROFIL -->
+			<div class="swiper-entry slider-2" id="profil">
+				<div class="swiper-container full-screen-height" data-direction="vertical" data-parallax="1" data-mousewheel="1" data-speed="1000">
+					<div class="swiper-button-prev"></div>
+					<div class="swiper-button-next"></div>
+					<div class="swiper-wrapper main">
+					<!-- INTRO -->
+						<div class="swiper-slide intro hidden-md hidden-lg"> 
+							<div class="container-fluid wide">
+								<div class="title-wrapper valign-middle">
+									<div class="content">
+										<div class="page-artists loreen-title">
+											<svg class="coversvg" width="100%" height="100%" >
+												<rect x="5%" y="5%" width="90%" height="90%"/>
+											</svg>
+											<div class="preview">
+												<div data-swiper-parallax-y="-75%" class="text-mask" style="background-image: url(img/loreen-fond.png); background-position: 15vw center;">
+													<div data-swiper-parallax-y="50%" class="text" >
+														<div class="position-block col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+															<div data-swiper-parallax-y="50%" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+																<img src="img/loreen-typo.png" alt="Loreen typography" />
+															</div>																													
+															<div data-swiper-parallax-y="50%" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 h1 title text-center" style="color: #AFAFAF;">
+																<h2 class="ht-1 black">LOREEN</h2>
+															</div>		
+														</div>	
+													</div>
+												</div>
+											</div>											
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					<!-- PROFIL -->
+						<div class="swiper-slide profil">
+							<div class="artist-cover" style="background-image: url('img/loreen-profil.jpg');"></div>
+							<div class="container-fluid wide">
+								<div class="title-wrapper valign-middle">
+									<div class="content col-md-7 hidden-xs hidden-sm">						
+									</div>
+									<div class="content col-md-5 col-xs-12">
+										<p data-swiper-parallax-y="-100%" class="sa text-left">#TATTOO - #ILLUSTRATION</p>
+										<div data-swiper-parallax-y="-150%"class="h1 title text-left" style="color: #FFFFFF;">
+											<span class="ht-1">
+												<h3>Loreen</h3>
+											</span>
+										</div>
+										<div data-swiper-parallax-y="50%" id="instafeed" class="col-md-12 col-sm-12  hidden-xs"></div>
+										<p data-swiper-parallax-y="-100%" class="sa text-justify">
+											C’est aprés quelques années en tant que graphiste qu’il se lancera dans l’aventure du tatouage sous l’impulsion de Piero en devenant  son apprenti. Son style est souvent qualifié de graphique vu qu’il aime les dessins gardant le côté crayonné du croquis et les couleurs aquarelles. Aujourd’hui il se lance dans des pièces plus ambitieuses en amenant un dessin presque réaliste dans ses compositions.
+										</p>
+										<h3 data-swiper-parallax="-100%" class="sl ls">
+											<a target="_blank" title="Instagram loreen" href="https://www.instagram.com/loreen2l/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+										</h3>
+									</div>
+								</div>
+							</div>
+						</div>
+					<!-- FOLIO -->
+						<?php include("loreen.html"); ?>	
+					</div>
+					<div class="swiper-pager ">							
+						<div class="swiper-pager-current">01</div>
+						<div class="swiper-pager-total">0</div>
+						<div class="swiper-pager-arrow-prev"></div>
+						<div class="swiper-pager-arrow-next"></div>
+					</div>
+				</div>
+				<div class="portfolio-eyes">
+				  <canvas></canvas>
+				</div>
+			</div>
+
+		</div>
+		<!-- Mes chaussures TN requin -->
+		<div class="cd-transition-layer"> 
+			<div class="bg-layer"></div>
+		</div>
+		<div class="phone-marker visible-xs"></div>
+		<div class="tablet-marker visible-sm"></div>
+		<!-- FW -->	
+		<script src="../assets/js/pace.js"></script>
+		<script src="../assets/js/jquery-2.2.4.min.js"></script>
+		<script src="../assets/js/swiper.jquery.min.js"></script>
+		<script src="../assets/js/sliceslider.jquery.js"></script>
+		<script src="../assets/js/jquery.mousewheel.min.js"></script>    
+		<script src="../assets/js/sliceslider.jquery.js"></script>
+		<!-- FOLIO -->
+		<script src="../assets/js/simple-lightbox.min.js"></script>
+		<script src="../assets/js/tilt.jquery.js"></script>
+		<!-- HAND -->
+		<script src="../assets/js/main.js"></script>
+		<script src="../assets/js/global.js"></script>
+		<!-- INSTAGRAM -->
+		<script type="text/javascript">		
+			var token = '559910664.ca4c424.05add8873c34498e84f6d3106ed1c5b4', num_photos = 6;
+			$.ajax({
+				url: 'https://api.instagram.com/v1/users/self/media/recent',
+				dataType: 'jsonp',
+				type: 'GET',
+				data: {access_token: token, count: num_photos},
+				success: function(data){
+					console.log(data);
+					for( x in data.data ){ $('#instafeed').append('<div class="presentation-entry col-sm-2 col-md-2"><a class="mouseover-3"><img alt="'+data.data[x].user.username+' instagram '+data.data[x].type+'" src="'+data.data[x].images.low_resolution.url+'"/><span class="valign-middle-content"><i class="fa fa-heart" aria-hidden="true"></i> '+data.data[x].likes.count+'</span></a></div>');
+					}
+				},
+				error: function(data){ console.log(data); }
+			});
+		</script>							
+	</body>
+</html>
