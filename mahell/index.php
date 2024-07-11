2fc95d4a0bbc84f91f57340db20f0c85096ecdf6
Yoann PIKS-L <50371128+PuxianAlHazred@users.noreply.github.com>
2024-07-11 14:58:56 +0200

    Add files via upload

---
+++ mahell/index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="fr" id="scroll">
+	<head>
+	<!-- Meta de qualité -->
+		<title>MAHELL - LA COUR DES MIRACLES</title>
+		<meta name="description" content="Mahell travaille à la Cour des Miracles de Toulouse où il est connu pour ses petits tatouages noir et blanc. Il commence aujourd'hui à utiliser la couleur et à tatouer des pièces plus importantes." />
+		<meta name="author" content="Piks-L" />	
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, minimal-ui"/>
+		 <meta charset="UTF-8">
+	<!-- Meilleur Favicon européen en 2017 élu à l'unanimité -->
+		<meta name="apple-mobile-web-app-title" content="LA COUR DES MIRACLES">
+		<meta name="application-name" content="LA COUR DES MIRACLES">
+		<meta name="msapplication-TileColor" content="#000000">
+		<meta name="msapplication-TileImage" content="../assets/img/favicons/fmstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		<link rel="shortcut icon" href="http://www.lacourdesmiracles.tattoo/assets/img/favicon.ico" />
+		<link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/fapple-touch-icon.png">
+		<link rel="icon" type="image/png" href="../assets/img/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="../assets/img/favicons/ffavicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="../assets/img/favicons/fmanifest.json">
+		<link rel="mask-icon" href="../assets/img/favicons/fsafari-pinned-tab.svg" color="#000000">
+	<!-- Les livres de la librairie à rendre avant mercredi -->	
+		<link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/bootstrap.extension.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/swiper.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/simplelightbox.css" rel="stylesheet" type="text/css" />
+
+		
+	<!-- Ma collection de Dragon Ball -->	
+		<link href="../assets/css/style.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/resette.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/stylette.css" rel="stylesheet" type="text/css" />
+		<link href="../assets/css/mobile.css" rel="stylesheet" type="text/css" />
+	<!-- Jeans tout neuf pour une belle soirée -->			
+		<script src="../assets/js/modernizr.js"></script>
+
+	<!-- Mes informations Tinder -->		
+		<meta property="og:site_name" content="LA COUR DES MIRACLES"/>
+		<meta property="og:title" content="MAHELL - LA COUR DES MIRACLES"/>
+		<meta property="og:type"   content="profile" /> 
+		<meta property="og:image" content="http://www.lacourdesmiracles.tattoo/mahell/img/mahell-profil.jpg"/>
+		<meta property="og:description" content="Mahell gravite autour du monde de l’illustration et du tatouage depuis les 2000's mais fait partie de l'équipe depuis quelques années maintenant. Il est connu pour ses pièces en noir et blanc, mais son style reste cependant en perpétuelle évolution. Il commence à utiliser la couleur dans ses tattoos tout en proposant des travaux plus complexes. "/>
+	<!-- Description de la cible à abattre ! -->			
+		<script type="text/javascript">
+			var _paq = _paq || [];
+			_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+			_paq.push(["setCookieDomain", "*.lacourdesmiracles.tattoo"]);
+			_paq.push(["setDomains", ["*.lacourdesmiracles.tattoo"]]);
+			_paq.push(["disableCookies"]);
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+				(function() {
+					var u="//piwik.piks-l.com/";
+					_paq.push(['setTrackerUrl', u+'piwik.php']);
+					_paq.push(['setSiteId', '3']);
+					var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+					g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+				})();
+		</script>
+	<!-- Pour les cassos -->	
+		<noscript><p><img src="//piwik.piks-l.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>	
+	</head>
+	<body>
+
+		<?php include("../header.php"); ?>
+		<div id="app">
+		<div id="profil" class="intro hidden-md hidden-lg"> 
+			<div class="container-fluid wide">
+				<div class="title-wrapper valign-middle">
+					<div class="content">
+						<div class="page-artists mahell-title">
+							<svg class="coversvg" width="100%" height="100%" >
+								<rect x="5%" y="5%" width="90%" height="90%"/>
+							</svg>
+							<div class="preview">
+								<div class="text-mask" style="background-image: url(img/mahell-fond.png); background-position: 15vw center;">
+									<div class="text" >
+										<div class="position-block col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+											<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+												<img src="img/mahell-typo.png" alt="Mahell typography" />
+											</div>																													
+											<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 h1 title text-center" style="color: #AFAFAF;">
+												<h2 class="ht-1 black">{{ json.data[0].nom }}</h2>
+											</div>		
+										</div>	
+									</div>
+								</div>
+							</div>											
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class=" profil" >
+			<div class="artist-cover" style="background-image: url('img/mahell-profil.jpg');"></div>
+			<div class="container-fluid wide">
+				<div class="title-wrapper valign-middle">
+					<div class="content col-md-7 hidden-xs hidden-sm">						
+					</div>
+					<div class="content col-md-5 col-xs-12">
+						<p class="sa text-left"> {{ json.data[0].tag }}	</p>
+						<div class="h1 title text-left" style="color: #FFFFFF;">
+							<span class="ht-1">
+								<h3>{{ json.data[0].nom }}</h3>
+							</span>
+						</div>
+						<div id="instafeed" class="col-md-12 col-sm-12  hidden-xs"></div>
+						<p class="sa text-justify">{{ json.data[0].description }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- FOLIO -->
+		<div class="folio" >
+			<ul class="flex-container">
+						
+						
+				<div v-for="item in json.data[0].galleries.data" :key="item.data" class="sorting-item col-xs-6 w25 filter-4 flex-item">
+					<div class="portfolio-preview-1">
+						<a style="transform-style: preserve-3d;" v-bind:href="'https://lacourdesmiracles.tattoo' +item.url" v-bind:title="item.title" class="lightbox mouseover-1">
+							<img class="preview" v-bind:src="'https://lacourdesmiracles.tattoo' +item.thumbnail_url" alt="tattoo mahell la cour des miracles" />
+							<svg class="svgtilt" width="100%" height="100%">
+								<rect x="5%" y="5%" width="90%" height="90%" />
+							</svg> 
+						</a>
+					</div>
+				</div>						
+																				
+			</ul>
+		</div>
+	
+		<div class="cd-transition-layer"> 
+			<div class="bg-layer"></div>
+		</div>
+		<div class="phone-marker visible-xs"></div>
+		<div class="tablet-marker visible-sm"></div>
+		<div class="content-shop content-blog"></div>
+	</div>
+
+		<!-- FW -->	
+		<script src="../assets/js/pace.js"></script>
+		<script src="../assets/js/jquery-2.2.4.min.js"></script>
+		<script src="https://unpkg.com/vue"></script>
+		<script type="text/javascript">	
+			getAPI = "../admin/api/1.1/tables/";
+			tokenAPI = "/rows.json?access_token=9ODAjZkEbDo6X80rrkaoUA3JEQAhtjfr";	
+
+			$.ajax({
+				url: getAPI + 'mahell' + tokenAPI,
+				method: 'GET',
+				success: function (json) { galleries.json = json;}
+			});
+			galleries = new Vue({
+				el: '#app',
+				data () { return {	
+					json:0
+				}}
+			})		
+		</script>	
+ 
+		<script src="../assets/js/swiper.jquery.min.js"></script>
+		<script src="../assets/js/sliceslider.jquery.js"></script>
+		<script src="../assets/js/jquery.mousewheel.min.js"></script>    
+		<script src="../assets/js/sliceslider.jquery.js"></script>
+		
+		<!-- FOLIO -->
+		<script src="../assets/js/simple-lightbox.min.js"></script>
+		<script src="../assets/js/tilt.jquery.js"></script>
+		<!-- HAND -->
+		<script src="../assets/js/main.js"></script>
+		<script src="../assets/js/global.js"></script>
+
+				
+		<script type="text/javascript">
+			var token = '32011074.ca4c424.acc28199b5634365aab90eae0d48b2df', num_photos = 6;
+			$.ajax({
+				url: 'https://api.instagram.com/v1/users/self/media/recent',
+				dataType: 'jsonp',
+				type: 'GET',
+				data: {access_token: token, count: num_photos},
+				success: function(data){
+					console.log(data);
+					for( x in data.data ){ $('#instafeed').append('<div class="presentation-entry col-sm-2 col-md-2"><a class="mouseover-3"><img alt="'+data.data[x].user.username+' instagram '+data.data[x].type+'" src="'+data.data[x].images.low_resolution.url+'"/><span class="valign-middle-content text-center"><i class="fa fa-heart" aria-hidden="true"></i> '+data.data[x].likes.count+'</span></a></div>');
+					}
+				},
+				error: function(data){ console.log(data); }
+			});
+		</script>	
+	</body>
+</html>
